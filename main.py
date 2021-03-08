b146044582d9a0ce6a91c63796b44a3884d4c073
Brian James Celine <brianceline@gmail.com>
2021-03-08 13:58:21 +0000

    Updated

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 # From File import Class
 from animal import Animal 
 
+dog = Animal(name = 'Lizzy', age = 3)
+cat = Animal(name = 'Zorka', age = 10)
+bird = Animal(name = 'Fufu', age = 5)
+
+print(dog.speak())
+print(cat.speak())
+print(bird.speak())
+
+print(dog.intro())
+print(cat.intro())
+print(bird.intro())
+
+
+
 ### print(dir(Animal)) to check methods which can be used.
 ### Returns a fault
 # print (Animal.speak())
@@ -18,16 +32,15 @@ from animal import Animal
 # -------------------------
 
 # To progress further from above
-
 # instance / object
-dog = Animal(name = 'Lizzy', age = 3)
-cat = Animal(name = 'Zorka', age = 10)
-bird = Animal(name = 'Fufu', age = 5)
+# dog = Animal(name = 'Lizzy', age = 3)
+# cat = Animal(name = 'Zorka', age = 10)
+# bird = Animal(name = 'Fufu', age = 5)
 
-print(dog.speak())
-print(cat.speak())
-print(bird.speak())
+# print(dog.speak())
+# print(cat.speak())
+# print(bird.speak())
 
-print(dog.intro())
-print(cat.intro())
-print(bird.intro())
+# print(dog.intro())
+# print(cat.intro())
+# print(bird.intro())
